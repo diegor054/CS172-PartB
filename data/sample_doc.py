9ee1cd3824661c0d1417614aa0b85489dedb2833
zdamm001 <zdamm001@ucr.edu>
2023-06-10 12:54:28 -0700

    added data folder with sample data

---
+++ data/sample_doc.py
@@ -0,0 +1,14 @@
+[
+    {
+        'title' : 'A',
+        'context' : 'lucene is a useful tool for searching and information retrieval'
+        },
+    {
+        'title' : 'B',
+        'context' : 'Bert is a deep learning transformer model for encoding textual data'
+    },
+    {
+        'title' : 'C',
+        'context' : 'Django is a python web framework for building backend web APIs'
+    }
+]      
